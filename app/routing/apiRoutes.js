23c9ea724755388cda129a855db0d8f61fa52860
Maddy Ford <mrhf008@gmail.com>
2017-10-15 12:03:46 -0700

    buttons routing correctly, labels updated, not printing new survey results yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,33 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends");
 // var waitListData = require("../data/waitinglistData");
 
 module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+	res.json(friends);
+})
+
 
 app.get("/api/home", function(req, res) {
 // res.json(Data);
 console.log("friends!");
 });
 
+
 app.get("/api/survey", function(req, res) {
 res.json(friendData);
 })
+
+
+app.post("/api/friends", function(req, res) {
+	friendArray.push(req.body);
+	res.json(true);
+});
+
+app.post("/api/clear", function() {
+	friends = [];
+
+	console.log("Cleared: " + friends);
+});
+
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,12 @@ res.sendFile(path.join(__dirname, "/../public/home.html"));
 app.get("/survey", function(req, res) {
 res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
-  // If no matching route is found default to home
+  
+
+// app.get("/friends", function(req, res) {
+// res.sendFile(path.join(_dirname, "/../data/friends.js"))
+// });
+// If no matching route is found default to home
 app.use(function(req, res) {
 res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
