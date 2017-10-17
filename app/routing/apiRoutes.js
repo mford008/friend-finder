6f39dcc1f3cf5a5a52351b39b71e4bc87f4f5acb
Maddy Ford <mrhf008@gmail.com>
2017-10-17 13:35:14 -0700

    added preliminary logic for best friend calculation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,6 +23,34 @@ app.post("/api/friends", function(req, res) {
 	friends.push(req.body);
 	res.json(true);
 // where the caculation should be done
+var bestMatch = {
+	name: "",
+	picture: "",
+	friendDifference: 0
+}
+
+var newFriend = req.body;
+var newFriendName = newFriend.name;
+var newFriendPicture = newFriend.picture;
+var newFriendScores = newFriend.scores;
+
+var totalDifference = 0;
+
+for (var i = 0; i < friends.length; i++) {
+	console.log(friends[i].name);
+		totalDifference = 0;
+
+		for (var j = 0; j < friends[i].scores[j]; j++) {
+			totalDifference += Math.abs((newFriendScores[j]) - (friends[i].scores[j]));
+		}
+}
+
+bestMatch.name = friends[i].name;
+bestMatch.picture = friends[i].picture;
+bestMatch.friendDifference = totalDifference;
+
+friends.push(newFriend);
+res.json(bestMatch);
 });
 
 
