9a82365047a2b634518eae98d30fc0dfb42e1052
Maddy Ford <mrhf008@gmail.com>
2017-10-19 19:18:16 -0700

    getting totalDifference for each individual friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -28,18 +28,35 @@ var newFriend = req.body;
 // var newFriendName = newFriend.name;
 // var newFriendPicture = newFriend.picture;
 var newFriendScores = newFriend.scores;
-
+var keys = ['question_one',
+            'question_two',
+            'question_three',
+            'question_four',
+            'question_five',
+            'question_six',
+            'question_seven',
+            'question_eight',
+            'question_nine',
+            'question_ten',
+            ];
+var totalDifference = []
 for (var i = 0; i < friends.length; i++) {
-	console.log(friends[i].name); //working
-		var totalDifference = 0;
+console.log(friends);
+  console.log(friends[i].name); //working
+		// var totalDifference = 0;
+    totalDifference.push(0);
 
 		for (var j = 0; j < friends[i].scores.length; j++) {
-			totalDifference += Math.abs(Number(newFriendScores[j]) - Number(friends[i].scores[j]));
+		// for (var j = 0; j < keys.length; j++) {
+      // var key = keys[j]
+			totalDifference[i] += Math.abs(Number(newFriendScores[j]) - Number(friends[i].scores[j]));
+			// totalDifference[i] += Math.abs(Number(newFriendScores[key]) - Number(friends[key]));
 			console.log(friends[i].scores);
       console.log('difference:', totalDifference);
 }
-      bestMatch.name = friends[i].name;
-      // bestMatch.name = friendsArray[i].name;
+}
+      // bestMatch.name = friends[i].name;
+      bestMatch.name = friendsArray[i].name;
       console.log('hello', bestMatch.name) //working
       bestMatch.picture = friends[i].picture;
       if (totalDifference <= bestMatch.friendDifference) {
@@ -51,7 +68,7 @@ for (var i = 0; i < friends.length; i++) {
       res.json(bestMatch);
       console.log('meow', bestMatch);//not logging
     // }
-}
+// }
 });
 
 // clearing out array
