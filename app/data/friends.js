07cf97ca40f5502c5e88e00a942303aa089d7d2e
Maddy Ford <mrhf008@gmail.com>
2017-10-21 19:31:33 -0700

    corrected filepath

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friendsArray = [
   {
     name: 'Maddy',
-    picture: '../images/halloween-kitten.jpg',
+    picture: '../../images/halloween-kitten.jpg',
     scores: [5, 4, 3, 4, 4, 5, 5, 1, 4, 3]
   },
   {
     name: 'Eloise',
-    picture: '../images/angry-kitten.jpg',
+    picture: '../../images/angry-kitten.jpg',
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   }
 ];
