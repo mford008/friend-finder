7f21a1829d238823ea019432051a51ce87617224
Maddy Ford <mrhf008@gmail.com>
2017-10-21 19:24:07 -0700

    added images from folder, looked nicer than long web addresses

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friendsArray = [
   {
     name: 'Maddy',
-    picture: 'http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg',
+    picture: '../images/halloween-kitten.jpg',
     scores: [5, 4, 3, 4, 4, 5, 5, 1, 4, 3]
   },
   {
     name: 'Eloise',
-    picture: 'https://img.huffingtonpost.com/asset/582cb2601a00002400cc8426.jpeg?cache=wltod2kvw5&ops=1910_1000',
+    picture: '../images/angry-kitten.jpg',
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   }
 ];
