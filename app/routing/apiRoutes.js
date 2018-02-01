efcebb51b77c11b51804bf53a88267b4b73343e8
Maddy Ford <mrhf008@gmail.com>
2018-02-01 15:50:38 -0800

    selects match correctly

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,60 +3,30 @@ module.exports = function (app) {
   app.get('/api/friends', function (req, res) {
     res.json(friends);
   });
-
-  // app.get('/api/survey', function (req, res) {
-    // res.json(friends);
-  // });
-
   app.post('/api/friends', function (req, res) {
-    // friends.push(req.body);
-    // res.json(true);
+    friends.push(req.body);
 
-    var newFriend = req.body;
-    var newFriendScores = newFriend.scores;
+    var currentScore = req.body.scores;
+    var bestFriend = 0;
+    var lowestScore = 50;
 
-    // var totalDifference = [];
-    var bestMatchName = '';
-    var bestMatchPic = '';
-    var totalDifference = 50;
+    for (var i = 0; i < (friends.length - 1); i++) {
+      var compScore = friends[i].scores;
+      var totalDifference = 0;
 
-    for (var i = 0; i < friends.length; i++) {
-      // totalDifference.push(0);
-      var diff = 0;
-      for (var j = 0; j < newFriendScores.length; j++) {
-        // totalDifference[i] += Math.abs(Number(newFriendScores[j]) - Number(friends[i].scores[j]));
-        diff += Math.abs(friends[i].scores[j] - newFriendScores[j]);
+      for (var j = 0; j < currentScore.length; j++) {
+        var singleDifference = Math.abs(parseInt(currentScore[j]) - parseInt(compScore[j]));
+        totalDifference = totalDifference + singleDifference;
       }
-      if (diff < totalDifference) {
-        totalDifference = diff;
-        bestMatchName = friends[i].name;
-        bestMatchPic = friends[i].photo;
+
+      if (totalDifference < lowestScore) {
+        lowestScore = totalDifference;
+        bestFriend = i;
       }
-      friends.push(newFriend);
-      res.json({status: 'OK', bestMatchName: bestMatchName, bestMatchPic: bestMatchPic});
     }
-  });
-// PSEUDOCODE
-// if minValue of totalDifference is equal to a friend's total difference, that friend is the best match
-// function findBestMatch() {
-//   if (minValue === totalDifference[i])//not working
-//     console.log(friends[i].name)
-//   }
-// }
-// findBestMatch();
-// Let bestMatch.name = the name of the friend with the minimum totalDifference value:
-// var bestMatch = {
-//   name: '',
-//   picture: '',
-//   totalDifference: ''
-// };
-// push that value to the header section of the modal:
-// ('#resultsModal').prepend(bestMatch.name)
-// (code must still work for new friends with lower totalDifference)
-    // friends.push(newFriend);
-    // res.json(bestMatch);
-  // });
 
+    res.send(friends[bestFriend]);
+  });
   app.post('/api/clear', function () {
     friends = [];
     console.log('Cleared: ' + friends);
