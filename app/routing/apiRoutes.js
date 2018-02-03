a3c105b0b11922b5194fd058ab45f57128a6add1
Maddy Ford <mrhf008@gmail.com>
2018-02-03 11:42:11 -0800

    moved js to separate file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,33 @@
 var friends = require('../data/friends');
+
 module.exports = function (app) {
   app.get('/api/friends', function (req, res) {
     res.json(friends);
   });
+
   app.post('/api/friends', function (req, res) {
     friends.push(req.body);
 
     var currentScore = req.body.scores;
+    // bestFriend will have lower margin of difference
     var bestFriend = 0;
+    // Ten questions options 1 - 5, 10 x 5 = 50 is the highest margin of difference
     var lowestScore = 50;
 
+    // Iterating over the friends in data
     for (var i = 0; i < (friends.length - 1); i++) {
+      // Score to compare to
       var compScore = friends[i].scores;
       var totalDifference = 0;
 
+      // Iterating over scores for each friend in data
       for (var j = 0; j < currentScore.length; j++) {
+        // Taking individual difference in survey answers - absolute value of current minus comparison score
         var singleDifference = Math.abs(parseInt(currentScore[j]) - parseInt(compScore[j]));
+        // Adding to total difference
         totalDifference = totalDifference + singleDifference;
       }
-
+      // If total difference is lower than 50, best friend is at current index
       if (totalDifference < lowestScore) {
         lowestScore = totalDifference;
         bestFriend = i;
@@ -27,6 +36,7 @@ module.exports = function (app) {
 
     res.send(friends[bestFriend]);
   });
+  // Clears friends from api
   app.post('/api/clear', function () {
     friends = [];
     console.log('Cleared: ' + friends);
