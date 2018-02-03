e26677ae814aecd8cfb892a4b2afe6bb82326e25
Maddy Ford <mrhf008@gmail.com>
2018-02-03 11:00:28 -0800

    images displaying correctly

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friendsArray = [
   {
     name: 'Maddy',
-    picture: '../../images/halloween-kitten.jpg',
+    picture: 'https://rlv.zcache.com/pumpkin_kitten_halloween_card-r051be55fbe9d4e66b652a67bcfa91955_xvuat_8byvr_540.jpg',
     scores: [5, 4, 3, 4, 4, 5, 5, 1, 4, 3]
   },
   {
     name: 'Eloise',
-    picture: '../../images/angry-kitten.jpg',
+    picture: 'https://i.ytimg.com/vi/Uzd9j2r7Hm8/maxresdefault.jpg',
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   }
 ];
