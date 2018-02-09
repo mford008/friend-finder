78d1675658b112ea004fc6212748d76c8b1362d1
Maddy Ford <mrhf008@gmail.com>
2018-02-09 15:01:47 -0800

    added new friend data for stranger things

--- data/friends.js
+++ data/friends.js
@@ -1,13 +1,28 @@
 var friendsArray = [
   {
-    name: 'Maddy',
-    picture: 'https://rlv.zcache.com/pumpkin_kitten_halloween_card-r051be55fbe9d4e66b652a67bcfa91955_xvuat_8byvr_540.jpg',
-    scores: [5, 4, 3, 4, 4, 5, 5, 1, 4, 3]
+    name: 'The Demogorgon',
+    picture: 'https://vignette.wikia.nocookie.net/animaljam/images/8/8a/Demogorgon.jpg/revision/latest?cb=20171122032931',
+    scores: [5, 1, 1, 5, 1, 3, 1, 5, 1, 5]
   },
   {
-    name: 'Eloise',
-    picture: 'https://i.ytimg.com/vi/Uzd9j2r7Hm8/maxresdefault.jpg',
-    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    name: 'Nancy',
+    picture: 'https://vignette.wikia.nocookie.net/strangerthings8338/images/7/7f/NancyWheeler.png/revision/latest?cb=20160819170317',
+    scores: [3, 1, 4, 3, 5, 5, 3, 3, 2, 3]
+  },
+  {
+    name: 'Dustin',
+    picture: 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1492110399/articles/2016/08/14/stranger-things-star-gaten-matarazzo-on-dustin-tv-s-most-lovable-toothless-geek/160813-_Leon-Gaten-Matarazzo-tease_orytly',
+    scores: [1, 5, 1, 3, 4, 5, 2, 5, 5, 5]
+  },
+  {
+    name: 'Mike',
+    picture: 'https://vignette.wikia.nocookie.net/strangerthings8338/images/4/48/Mike_Wheeler_S1.png/revision/latest?cb=20171025205131',
+    scores: [2, 5, 2, 5, 5, 5, 2, 5, 5, 1]
+  },
+  {
+    name: 'Steve',
+    picture: 'https://vignette.wikia.nocookie.net/strangerthings8338/images/7/78/Image.jpg/revision/latest?cb=20171113113237',
+    scores: [4, 1, 5, 3, 4, 3, 5, 1, 1, 2]
   }
 ];
 
